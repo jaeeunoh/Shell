e801ecfe8c598a183dc345ddadf61db948452d0c
Jae Eun Oh <ohjaeeun@cs.grinnell.edu>
2017-01-31 09:18:11 -0600

    working on partB

--- mysh.c
+++ mysh.c
@@ -30,16 +30,32 @@ int main(int argc, char** argv) {
       }
     }
 
-    
     char** inputs = (char**) malloc (strlen(line)); 
     char* word = strtok (line, " ");
     while (word != NULL) {
       inputs[counter] = word;
-      printf ("word is %s \n", inputs[counter]); 
+      //printf ("word is %s \n", inputs[counter]); 
       counter++;
       word = strtok (NULL, " ");
     }
-    
+    // Append null terminator
+    inputs[counter] = NULL;  
+
+    pid_t child_id = fork();
+
+    int status;
+    int exec_status;
+    if (child_id == 0) {
+      exec_status = execvp (inputs[0], inputs);
+      if (exec_status < 0) {
+        perror("Error");
+        exit(2);
+      }
+    } else {
+      wait(&status);
+      printf ("Child process %d exited with status %d\n", child_id, status); 
+    } 
+      
     free(line);
   }
   
