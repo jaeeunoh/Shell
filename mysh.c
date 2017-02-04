6756094ee2937e3e39f9cc2cae4d510e7ce6e2ee
jaeeunoh <jaeeunoh@JaeEuns-MacBook-Air.local>
2017-02-04 15:43:20 -0600

    implemented commands to run multiple command lines

--- mysh.c
+++ mysh.c
@@ -14,6 +14,7 @@ int main(int argc, char** argv) {
     char* line = NULL;    // Pointer that will hold the line we read in
     size_t line_length;   // Space for the length of the line
     int counter = 0;  
+    int inital_counter = 0; 
     
     // Print the shell prompt
     printf("> ");
@@ -30,44 +31,70 @@ int main(int argc, char** argv) {
       }
     }
 
-    char** inputs = (char**) malloc (strlen(line)); 
-    char* word = strtok (line, " \n;");
-
-    //Execute changing directory function in parent process
-    if (strcmp (word, "cd") == 0) {
-      word = "chdir";
-      int error_msg = chdir(strtok(NULL, " \n;"));
-      if (error_msg < 0) {
-        perror("Error");
+    // Check to see if the input contains a semi colon
+    if (strchr (line, ';')) { 
+      char** inputsWithSemicolon = (char**) malloc (strlen(line)); 
+      char* tokenizeBySemicolon = strtok (line, ";\n");
+      while (tokenizeBySemicolon != NULL) { 
+        printf("%s\n", tokenizeBySemicolon);
+        inputsWithSemicolon[inital_counter] = tokenizeBySemicolon; 
+        inital_counter++;
+        tokenizeBySemicolon = strtok (NULL, ";\n");   
+      }
+      int i;
+      for (i = 0; i < inital_counter; i++) { 
+         char* word = strtok (line, " \n"); 
+        char** inputs = (char**) malloc (strlen(line)); 
+        while (word != NULL) {    
+          inputs[counter] = word;
+          printf ("word is %s \n", inputs[counter]); 
+          counter++;
+          word = strtok (NULL, " \n");
+        } 
+        inputs[counter] = NULL;  
+        run_command(inputs); 
       }
     } else { 
-      while (word != NULL) {    
-        inputs[counter] = word;
-        printf ("word is %s \n", inputs[counter]); 
-        counter++;
-        word = strtok (NULL, " \n;");
-      }
-      // Append null terminator
-      inputs[counter] = NULL;  
-
-      pid_t child_id = fork();
-
-      int status;
-      int exec_status;
-      if (child_id == 0) {
-        exec_status = execvp (inputs[0], inputs);
-        if (exec_status < 0) {
+      char* word = strtok (line, " \n"); 
+      char** inputs = (char**) malloc (strlen(line)); 
+      if (strcmp (word, "cd") == 0) {
+        word = "chdir";
+        int error_msg = chdir(strtok(NULL, " \n"));
+        if (error_msg < 0) {
           perror("Error");
-          exit(2);
         }
-      } else {
-        wait(&status);
-        printf ("Child process %d exited with status %d\n", child_id, status); 
-      } 
-      
-      free(line);
+      } else { 
+        while (word != NULL) {    
+          inputs[counter] = word;
+          printf ("word is %s \n", inputs[counter]); 
+          counter++;
+          word = strtok (NULL, " \n");
+        }
+        //Append null terminator
+        inputs[counter] = NULL;  
+        run_command(inputs); 
+      }
     }
-  } 
-  
-  return 0;
-}
+free(line); 
+  }
+return 0; 
+} 
+
+  void run_command(char** args) { 
+    int status; 
+    pid_t child_id = fork();
+    int exec_status;
+    if (child_id == 0) {
+      exec_status = execvp (args[0], args);
+      if (exec_status < 0) {
+        perror("Error");
+        exit(2);
+      }
+    } else {
+      wait(&status);
+      printf ("Child process %d exited with status %d\n", child_id, status); 
+    } 
+  }
+
+
+   
